df6ef409278ea55f8a2a0771c015221ad0b2c439
Nico <nico.03727+github@gmail.com>
2024-09-01 14:29:44 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { Button } from "$lib/components/button";
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+  <h1 class="text-6xl font-bold">
+    Welcome to your new<br />SvelteKit app
+  </h1>
+
+  <Button color="primary">Primary</Button>
+</section>
