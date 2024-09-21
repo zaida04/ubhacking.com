acaac0c9256b5417b04cad6ebedf22bdf992ce9d
zaida04 <nico.03727+github@gmail.com>
2024-09-21 01:02:12 -0400

    feat: add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Button from "$lib/components/button/button.svelte";
+</script>
+
+<div class="min-h-[75vh] bg-white text-black flex flex-col items-center pt-20">
+  <div class="z-10">
+    <h1 class="text-6xl font-bold mb-4">404</h1>
+  </div>
+
+  <p class="text-xl mb-8 text-center z-10">
+    Oops! The page you're looking for doesn't exist.
+    <br />
+    Let's get you back to safety!
+  </p>
+
+  <div class="z-10">
+    <a href="/">
+      <Button variant="default">Go back home</Button>
+    </a>
+  </div>
+</div>
+
+<style>
+  .min-h-screen {
+    height: 100vh;
+  }
+</style>
