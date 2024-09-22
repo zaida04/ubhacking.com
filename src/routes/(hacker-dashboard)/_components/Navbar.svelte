464d30782a2e835eb0773ab0b7a79cfd9678dfba
zaida04 <nico.03727+github@gmail.com>
2024-09-22 00:21:43 -0400

    fix: some stuffs

---
+++ src/routes/(hacker-dashboard)/_components/Navbar.svelte
@@ -0,0 +1,28 @@
+<header class="pt-4 pb-2 px-4 border-b">
+  <nav class="flex justify-center space-x-4">
+    <a
+      href="/"
+      class="px-3 py-2 text-sm font-medium rounded-md hover:bg-gray-100 hover:text-gray-900"
+    >
+      Home
+    </a>
+    <a
+      href="/application"
+      class="px-3 py-2 text-sm font-medium rounded-md hover:bg-gray-100 hover:text-gray-900"
+    >
+      Your Application
+    </a>
+    <a
+      href="/find-team"
+      class="px-3 py-2 text-sm font-medium rounded-md hover:bg-gray-100 hover:text-gray-900"
+    >
+      Find a team
+    </a>
+    <a
+      href="/study-mode"
+      class="px-3 py-2 text-sm font-medium rounded-md hover:bg-gray-100 hover:text-gray-900"
+    >
+      Study Mode
+    </a>
+  </nav>
+</header>
