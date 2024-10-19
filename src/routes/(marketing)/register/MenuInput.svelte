ea7c9ada1057d92277024cebea0be86b65994bfc
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2024-10-19 14:18:42 -0700

    feat: menu theming

---
+++ src/routes/(marketing)/register/MenuInput.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+  export let label = "";
+  export let id = "";
+  export let type = "text";
+  export let name = "";
+  export let value = "";
+  export let error: string | string[] | undefined = "";
+  export let placeholder = "";
+  export let required = false;
+  export let options: any = []; // For select inputs
+  export let checked = false; // For checkbox inputs
+  export let onCheckedChange = (arg: any) => {}; // For checkbox inputs
+
+  function handleCheckboxChange(event: any) {
+    checked = event.target.checked;
+    onCheckedChange(checked);
+  }
+</script>
+
+<div class="flex items-baseline">
+  {#if required && type !== 'checkbox'}
+    <span class="text-red-400 italic mr-1">*</span>
+  {/if}
+  <span class="font-serif italic text-lg mr-2 text-gray-800">{label}</span>
+  {#if type !== 'checkbox'}
+    <div class="flex-grow border-b border-dotted border-gray-400"></div>
+  {/if}
+  {#if type === 'email'}
+    <input
+      type="email"
+      {id}
+      {placeholder}
+      {name}
+      bind:value
+      class="ml-2 rounded-sm bg-gray-50 border-gray-400 border p-2 font-serif"
+    />
+  {:else if type === 'password'}
+    <input
+      type="password"
+      {id}
+      {placeholder}
+      {name}
+      bind:value
+      class="ml-2 rounded-sm bg-gray-50 border-gray-400 border p-2 font-serif"
+    />
+  {:else if type === 'date'}
+    <input
+      type="date"
+      {id}
+      {placeholder}
+      {name}
+      bind:value
+      class="ml-2 rounded-sm bg-gray-50 border-gray-400 border p-2 font-serif"
+    />
+  {:else if type === 'number'}
+    <input
+      type="number"
+      {id}
+      {placeholder}
+      {name}
+      bind:value
+      class="ml-2 rounded-sm bg-gray-50 border-gray-400 border p-2 font-serif"
+    />
+  {:else if type === 'select'}
+    <select
+      {id}
+      {name}
+      bind:value
+      class="ml-2 rounded-sm bg-gray-50 border-gray-400 border p-2 font-serif"
+    >
+      {#each options as option}
+        <option value={option.value}>{option.label}</option>
+      {/each}
+    </select>
+  {:else if type === 'checkbox'}
+    <input
+      type="checkbox"
+      {id}
+      {name}
+      checked={checked}
+      on:change={handleCheckboxChange}
+      class="ml-2 rounded-sm bg-gray-50 border-gray-400 border p-2"
+    />
+  {:else}
+    <input
+      type="text"
+      {id}
+      {placeholder}
+      {name}
+      bind:value
+      class="ml-2 rounded-sm bg-gray-50 border-gray-400 border p-2 font-serif"
+    />
+  {/if}
+</div>
+{#if error}
+  <p class="text-red-500 text-sm mt-1 ml-[150px]">{error}</p>
+{/if}
