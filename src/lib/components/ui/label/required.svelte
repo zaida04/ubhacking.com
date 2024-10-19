903cf243b275a58341a7b051c11d736f6dfd070e
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2024-10-19 14:35:26 -0700

    feat: menu menu
    this should have been included

--- src/lib/components/ui/label/required.svelte
+++ src/lib/components/ui/label/required.svelte
@@ -1,8 +1,4 @@
-<div class="flex gap-2 items-center">
-  <slot />
-  <div class="bg-red-400 rounded-full px-2 w-fit h-fit">
-    <p class="text-white" style="font-size: 0.60rem; line-height: 1.0rem;">
-      Required
-    </p>
-  </div>
-</div>
+<span class="text-red-400 italic">
+*
+</span>
+<slot />
