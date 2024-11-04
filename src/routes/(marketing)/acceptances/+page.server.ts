889e49b13d224660c87135ef210b4376e005b69f
Nico <nico.03727+github@gmail.com>
2024-11-04 10:45:51 -0500

    Update +page.server.ts

--- src/routes/(marketing)/acceptances/+page.server.ts
+++ src/routes/(marketing)/acceptances/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
-const allowedUserIds = ["f3c19660-3c09-472a-83a8-6665643ee1bc", "c787ec03-0a0f-4c7e-8211-726a2d50fef0", "fc6694b6-ce85-4c23-87dd-d55cb92e6420", "54cacdcd-c987-475f-8a7c-54f482792dff"];
+const allowedUserIds = ["659a17ae-c3af-454e-84ec-1ce3fc372146", "f3c19660-3c09-472a-83a8-6665643ee1bc", "c787ec03-0a0f-4c7e-8211-726a2d50fef0", "fc6694b6-ce85-4c23-87dd-d55cb92e6420", "54cacdcd-c987-475f-8a7c-54f482792dff"];
 
 export const load: PageServerLoad = async ({ locals }) => {
     const session = await locals.safeGetSession();
